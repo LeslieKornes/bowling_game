e1e6f531d09b03b6948c85322074e155c2049a87
Leslie Kornes <lkornes@covermymeds.com>
2018-05-12 19:22:30 -0400

    Refactor code by adding spare? method for readability.

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -10,15 +10,20 @@ class BowlingGame
     first_in_frame = 0
 
     while frame < 10
-      if (@rolls[first_in_frame] + @rolls[first_in_frame + 1]) == 10
+      if spare?(first_in_frame)
         score += 10 + @rolls[first_in_frame + 2]
       else
         score += @rolls[first_in_frame] + @rolls[first_in_frame + 1]
       end
-      frame += 1
-      first_in_frame += 2
+        frame += 1
+        first_in_frame += 2
+      end
+      score
     end
-    score
-  end
+
+    private
+    def spare?(first_in_frame)
+      (@rolls[first_in_frame] + @rolls[first_in_frame + 1]) == 10
+    end
 
 end
