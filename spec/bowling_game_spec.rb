980a8b34ada985e5bb7674d1ea94704e4bf1108a
Leslie Kornes <lkornes@covermymeds.com>
2018-05-12 18:46:53 -0400

    Write failing test for game of 1s.

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -1,11 +1,16 @@
 require_relative '../lib/bowling_game'
 
 describe BowlingGame do
+  let (:game) { BowlingGame.new }
 
-  it 'scores a gutter game' do
-    game = BowlingGame.new
-    game.pins([0] * 20)
-    expect(game.score).to eq(0)
-  end
+    it 'scores a gutter game' do
+      game.pins([0] * 20)
+      expect(game.score).to eq(0)
+    end
+
+    it 'scores a game of 1s' do
+      game.pins([1] * 20)
+      expect(game.score).to eq(20)
+    end
 
 end
